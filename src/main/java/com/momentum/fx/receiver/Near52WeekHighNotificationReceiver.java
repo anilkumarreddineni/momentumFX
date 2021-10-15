3e1e45cdd67e6aa45d1b6b9364bf025dbacf682e
anilkumarreddineni <anilkumarreddineni@gmail.com>
2021-10-15 14:02:31 +0530

    changes made.

--- src/main/java/com/momentum/fx/receiver/Near52WeekHighNotificationReceiver.java
+++ src/main/java/com/momentum/fx/receiver/Near52WeekHighNotificationReceiver.java
@@ -1,5 +1,7 @@
 package com.momentum.fx.receiver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +12,15 @@ import javafx.application.Platform;
 
 @Component
 public class Near52WeekHighNotificationReceiver {
+	
+	private static final Logger log = LoggerFactory.getLogger(Near52WeekHighNotificationReceiver.class);
 
 	@JmsListener(destination = QueueBucket.NEAR_52_WEEK_HIGH_BUCKET)
 	public void receiveQueue(String message) {
-		System.out.println("Message Received: " + message);
+		log.info("Message received is  " + message);
 		Platform.runLater(() -> {
 			try {
-				Near52WeekHighNotification bearNotification=new Near52WeekHighNotification(message);
+				Near52WeekHighNotification bearNotification = new Near52WeekHighNotification(message);
 				bearNotification.showAndWait();
 			} catch (Exception ex) {
 				ex.printStackTrace();
